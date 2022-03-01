8e175d72d158f0bb25d58cc3a31ca53d29e4ec59
PeNgu1n4ik <mishkanednishka2@gmail.com>
2022-03-01 13:58:08 +0300

    кнопочки

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        "Привет 4 курс",
-        style: TextStyle(fontSize: 30, fontFamily: 'Roboto'),
-      ),
+  runApp(MyFirstApp());
+}
 
-    ),
-    body: Stack(
-      alignment: Alignment.center,
-      children: [
-        Icon(
-          Icons.airplay,
-          color: Colors.redAccent,
-          size: 200,
-        ),
-        Icon(
-          Icons.dangerous,
-          color: Colors.blue,
-          size: 150,
-        ),
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                "Привет 4 курс",
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+            body: Column(
+              children: [MyButton(), MyButton(), MyButton()],
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )));
+  }
+}
 
-        Image(
-          image: AssetImage("assets/images/cat.jpg"),
-          width: 400,
-          height: 400,
+class MyButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  int value = 0;
+  double fontSize = 22;
+  Color color = Colors.blue;
+  Color buttonFontColor = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: Text(
+          value <= 20 ? "Значение: $value" : "Больше 20",
+          style: TextStyle(fontSize: fontSize, color: buttonFontColor),
         ),
-        Positioned(
-          top: 250,
-             child: Text(
-              "Котябра Папича",
-              style: TextStyle(fontSize: 30, fontFamily: 'Roboto',color: Colors.amber),
-            )),
-      ],
-    ),
-  )));
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+        ),
+        onPressed: () {
+          setState(() {
+            value = value + 1;
+            if (value > 10) {
+              fontSize = 50;
+            }
+            if (value > 20) {
+              color = Colors.red;
+            }
+            if (value > 5) {
+              buttonFontColor = Colors.yellow;
+            }
+          });
+        });
+  }
 }
